4a9e0c42de200cb0badd2b445c653e7d6da5108a
Lorena Spiller <lorenaspiller@gmail.com>
2021-05-07 17:35:54 +0200

    added home main with sections, added pasta data array

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,10 +9,11 @@
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
         {{-- /google font --}}
-        
+
         <link rel="stylesheet" href="{{ asset('css/app.css') }}" >
     </head>
     <body>
+
         <header>
             <div class="container">
                 <div class="logo">
@@ -29,7 +30,41 @@
         </header>
 
         <main>
+            <div class="container">
+                <section>
+                    <h2 class="uppercase">Le lunghe</h2>
+                    <div class="cards">
+                        @foreach ($lunghe as $pasta)
+                            <div class="card">
+                                <img src="{{ $pasta['src']}}" alt="{{ $pasta['titolo']}}">
+                            </div> 
+                        @endforeach
+                    </div>
+                </section>
 
+                <section>
+                    <h2 class="uppercase">Le corte</h2>
+                    <div class="cards">
+                        @foreach ($corte as $pasta)
+                            <div class="card">
+                                <img src="{{ $pasta['src']}}" alt="{{ $pasta['titolo']}}">
+                            </div> 
+                        @endforeach
+                    </div>
+                </section>
+
+                <section>
+                    <h2 class="uppercase">Le cortissime</h2>
+                    <div class="cards">
+                        @foreach ($cortissime as $pasta)
+                            <div class="card">
+                                <img src="{{ $pasta['src']}}" alt="{{ $pasta['titolo']}}">
+                            </div> 
+                        @endforeach
+                    </div>
+                </section>
+
+            </div>
         </main>
 
         <footer>
