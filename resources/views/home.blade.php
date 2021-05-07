6e1b9d8cdef0bf3179a7c6443f88554f9ccd5a69
Lorena Spiller <lorenaspiller@gmail.com>
2021-05-07 16:49:46 +0200

    added images, header home page, google font

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,36 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>La Molisana</title>
+        {{-- google font --}}
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+        {{-- /google font --}}
+        
         <link rel="stylesheet" href="{{ asset('css/app.css') }}" >
     </head>
     <body>
+        <header>
+            <div class="container">
+                <div class="logo">
+                    <img src="{{ asset('img/marchio-sito-test.png') }}" alt="Logo La Molisana" >
+                </div>
+                <nav class="main-nav">
+                    <ul>
+                        <li class="active"><a href="#">Home</a></li>
+                        <li><a href="#">Prodotti</a></li>
+                        <li><a href="#">News</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
 
+        <main>
+
+        </main>
+
+        <footer>
+
+        </footer>
         
     </body>
 </html>
